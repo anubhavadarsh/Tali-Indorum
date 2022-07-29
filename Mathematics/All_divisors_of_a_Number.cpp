b7afa5541dffa01e547ab38383db46208ce2a3e2
anubhavadarsh <anubhav.adarsh9@gmail.com>
2022-07-29 12:35:44 +0530

    chore: define the purpose for last_divisor

--- Mathematics/All_divisors_of_a_Number.cpp
+++ Mathematics/All_divisors_of_a_Number.cpp
@@ -5,16 +5,16 @@ using namespace std;
 void  divisors(int n)
 {
     int i = 1;
-    int last_prime;
+    int last_divisor;
     for(; i*i < n; ++i)
     {
         if(n % i == 0){
             cout<<i<<' ';
-            last_prime = i;}
+            last_divisor = i;} //keep check of the last divisor
     }
     for(; i > 0; --i)
     {
-        if(n % i == 0 && last_prime != n/i)
+        if(n % i == 0 && last_divisor != n/i) //check if last divisor already printed
             cout<<n/i<<' ';
     }
 }
